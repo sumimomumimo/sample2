b54eb958cd26c82a6e7d555ee117cd8e7e04c0fd
sumimomumimo <87702872+sumimomumimo@users.noreply.github.com>
2021-07-22 22:59:45 +0900

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-  path('menu/', include('menu.urls')),
-  path('', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path('', include('menu.urls')),
+  
 ]
